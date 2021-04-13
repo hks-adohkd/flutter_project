da1aa8a549297d89fcda50cf1aa1cca16b1fe8ff
mhdYazbek <mohammed.yazbek.5@gmail.com>
2021-04-13 10:19:56 -0400

    added redeem screen and correct all margin and padding

--- open_sism/lib/screens/redeem_screen.dart
+++ open_sism/lib/screens/redeem_screen.dart
@@ -12,9 +12,20 @@ class RedeemScreen extends StatefulWidget {
   _RedeemScreenState createState() => _RedeemScreenState();
 }
 
+bool scrollBarShown() {
+  if (SizeConfig.orientation == Orientation.landscape) {
+    return true;
+  } else
+    return false;
+}
+
 class _RedeemScreenState extends State<RedeemScreen> {
+  bool scrollShown;
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    scrollShown = scrollBarShown();
+    //double defaultSize = SizeConfig.defaultSize;
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: kAppBarHeight,
@@ -50,32 +61,56 @@ class _RedeemScreenState extends State<RedeemScreen> {
                   child: Padding(
                     padding: EdgeInsets.symmetric(
                         horizontal: SizeConfig.defaultSize * 2),
-                    child: GridView.builder(
-                      itemCount: 1,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount:
-                            SizeConfig.orientation == Orientation.landscape
-                                ? 1
-                                : 1,
-                        mainAxisSpacing: 20,
-                        crossAxisSpacing:
-                            SizeConfig.orientation == Orientation.landscape
-                                ? SizeConfig.defaultSize * 1
-                                : 0,
-                        childAspectRatio: 1.65,
-                      ),
-                      itemBuilder: (context, index) => RecipeBundelCard(
-                        selectedGender: ScreenType.prize,
-                        recipeBundle: widget.prizeBundle,
-                        press: () {},
+                    child: Scrollbar(
+                      isAlwaysShown: scrollShown,
+                      hoverThickness: 10,
+                      thickness: 20,
+                      child: SingleChildScrollView(
+                        child: Container(
+                          height: 500,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              GridView.builder(
+                                scrollDirection: Axis.vertical,
+                                shrinkWrap: true,
+                                itemCount: 1,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: SizeConfig.orientation ==
+                                          Orientation.landscape
+                                      ? 2
+                                      : 1,
+                                  mainAxisSpacing: 20,
+                                  crossAxisSpacing: SizeConfig.orientation ==
+                                          Orientation.landscape
+                                      ? SizeConfig.defaultSize * 2
+                                      : 0,
+                                  childAspectRatio: 1.65,
+                                ),
+                                itemBuilder: (context, index) =>
+                                    RecipeBundelCard(
+                                  selectedGender: ScreenType.prize,
+                                  recipeBundle: widget.prizeBundle,
+                                  press: () {},
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                  'this is a gift card for google play this is a gift card for google play'),
+                              SizedBox(height: 20),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text('Redeem Reward'),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('hello'),
-                )
               ],
             ),
           ),
