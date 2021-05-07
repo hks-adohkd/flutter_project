3020c65e6b727472738d4155f68a7267ca5b859f
mhdYazbek <mohammed.yazbek.5@gmail.com>
2021-05-07 14:01:53 -0400

    Add firebase messaging to Application , receiving notification

--- open_sism/lib/screens/home/home_screen.dart
+++ open_sism/lib/screens/home/home_screen.dart
@@ -1,13 +1,93 @@
+import 'dart:convert';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:open_sism/configurations/constants.dart';
 import 'package:open_sism/screens/home/components/body.dart';
 import 'package:open_sism/components/appBar.dart';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:open_sism/configurations/token_monitor.dart';
+import '../../main.dart';
 
-class HomeScreen extends StatelessWidget {
+// Crude counter to make messages unique
+int _messageCount = 0;
+
+/// The API endpoint here accepts a raw FCM payload for demonstration purposes.
+String constructFCMPayload(String token) {
+  _messageCount++;
+  return jsonEncode({
+    'token': token,
+    'data': {
+      'via': 'FlutterFire Cloud Messaging!!!',
+      'count': _messageCount.toString(),
+    },
+    'notification': {
+      'title': 'Hello FlutterFire!',
+      'body': 'This notification (#$_messageCount) was created via FCM!',
+    },
+  });
+}
+
+class HomeScreen extends StatefulWidget {
   static String routeName = "/home";
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _token;
+  Stream<String> _tokenStream;
+  void setToken(String token) {
+    print('FCM Token: $token');
+    setState(() {
+      _token = token;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      if (message != null) {
+        print(message.data);
+      }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      print(notification.body);
+      if (notification != null && android != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                // TODO add a proper drawable resource to android, for now using
+                //      one that already exists in example app.
+                icon: 'launch_background',
+              ),
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {});
+    FirebaseMessaging.instance.getToken().then(setToken);
+    _tokenStream = FirebaseMessaging.instance.onTokenRefresh;
+    _tokenStream.listen(setToken);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(_token);
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: kAppBarHeight,
