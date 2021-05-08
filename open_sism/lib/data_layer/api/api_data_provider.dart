33a00c27a99e780daf5d395c07d98f5de164cbb1
mhdYazbek <mohammed.yazbek.5@gmail.com>
2021-05-08 14:14:35 -0400

    fixed locally connection with SQL server

--- open_sism/lib/data_layer/api/api_data_provider.dart
+++ open_sism/lib/data_layer/api/api_data_provider.dart
@@ -3,37 +3,33 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext context){
+  HttpClient createHttpClient(SecurityContext context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
 
 Future<http.Response> fetchCities() async {
   print("communicating");
-  var url ='https://10.0.2.2:44355/api/Cities/GetAll';
+  var url = 'http://10.0.2.2:1095/api/Cities/GetAll';
 
-  Map data = {
-    "page": 1,
-    "limit": 100
-  };
+  Map data = {"page": 1, "limit": 100};
 
   Map<String, String> headers = {
     "Content-Type": "application/json",
-    "Accept-Language" : "en",
-    "App-Version" : "1.0.0",
-    "Platform" : "Android",
+    "Accept-Language": "ar",
+    "App-Version": "1.0.0",
+    "Platform": "Android",
   };
 
   //encode Map to JSON
   var body = json.encode(data);
 
-  var response = await http.post(Uri.parse(url),
-      headers: headers,
-      body: body
-  );
+  var response = await http.post(Uri.parse(url), headers: headers, body: body);
+  print("tt");
   print("${response.statusCode}");
   print("${response.body}");
   return response;
